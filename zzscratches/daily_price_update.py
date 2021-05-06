cc27b5cfcac859396f8279a6cbd08741f21ecb1e
Viren Desai <virendesai87@gmail.com>
2021-05-06 14:09:26 -0500

    reorganized folder

---
+++ zzscratches/daily_price_update.py
@@ -0,0 +1,7 @@
+# BootStrapper OG scratch
+import prices as pr
+
+# --------------------------------- PRICE UPDATES ---------------------------------
+# >>> DAILY PRICE UPDATE
+pr.run_price_update(distribution_list='test')
+#pr.get_final_price_for_contract('wti',as_of_date='11/10/2020', contract_date='7/31/2020')
